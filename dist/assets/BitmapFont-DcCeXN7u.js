7427610f5a443e815b662a4b7b07d19a68841ab4
cen0b-dev <contact.cen0b.us@gmail.com>
2026-04-03 01:21:31 -0400

    Update dist asset filenames and imports
    Rebuilt distribution artifacts with new content hashes: several dist/assets files were renamed and their internal imports updated (e.g. index-QPjC7419.js → index-C2Smx5v5.js, CanvasPool, RenderTargetSystem, renderer bundles, etc.). Also removed dist/assets/mods/resource_packs/stratum-core/stratum-core.zip and updated dist/index.html. These changes reflect a fresh build output and ensure module import paths match the new hashed filenames.

--- dist/assets/BitmapFont-DcCeXN7u.js
+++ dist/assets/BitmapFont-DcCeXN7u.js
@@ -1,4 +1,4 @@
-import{D as nt,T as Z,C as U,M as ft,j as at,F as et,w as ht,E as Ft,b as Pt,o as X,t as pt,p as gt,q as j,v as N,r as St,x as rt,I as It,y as R,c as Ht}from"./index-QPjC7419.js";import{C as xt}from"./CanvasPool-D0xuXJ4E.js";/**
+import{D as nt,T as Z,C as U,M as ft,j as at,F as et,w as ht,E as Ft,b as Pt,o as X,t as pt,p as gt,q as j,v as N,r as St,x as rt,I as It,y as R,c as Ht}from"./index-C2Smx5v5.js";import{C as xt}from"./CanvasPool-BF0HIrSX.js";/**
  * tiny-lru
  *
  * @copyright 2026 Jason Mulligan <jason.mulligan@avoidwork.com>
